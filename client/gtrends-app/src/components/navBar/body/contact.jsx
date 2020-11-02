0b4c986ea62ca61cb3c235b0f2fb070510a81745
Juan Bautista Abal <keroxx3@gmail.com>
2020-11-01 23:39:17 -0300

    Corregido link linkedin

--- client/gtrends-app/src/components/navBar/body/contact.jsx
+++ client/gtrends-app/src/components/navBar/body/contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
             <h6 className='white-text'>Rosario, Santa Fe, Argentina</h6>
             <h6 className='white-text'>
                 <div>
-                    <a target="_blank" className='btn btn-contact transparent' href="https://www.linkedin.com/in/juan-bautista-abal-3618a5b5/">
+                    <a target="_blank" className='btn btn-contact transparent' href="https://www.linkedin.com/in/%F0%9F%87%A6%F0%9F%87%B7-juan-bautista-abal-3618a5b5/">
                         Linkedin:
                         <i className='material-icons white-text right'>insert_link</i>
                     </a>
